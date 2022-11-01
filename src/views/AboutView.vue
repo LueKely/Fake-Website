22e2639c9055f8fc6ea7442fc2a6df66d414b21f
LueKely <luekely43110@gmail.com>
2022-11-01 10:34:32 +0800

    Update AboutView.vue

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,15 +1,32 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+	<div class="about">
+		<h1>This is an about page</h1>
+		<div
+			class="border border-blue-300 shadow rounded-md p-4 max-w-sm w-full mx-auto"
+		>
+			<div class="animate-pulse flex space-x-4">
+				<div class="rounded-full bg-slate-700 h-10 w-10"></div>
+				<div class="flex-1 space-y-6 py-1">
+					<div class="h-2 bg-slate-700 rounded"></div>
+					<div class="space-y-3">
+						<div class="grid grid-cols-3 gap-4">
+							<div class="h-2 bg-slate-700 rounded col-span-2"></div>
+							<div class="h-2 bg-slate-700 rounded col-span-1"></div>
+						</div>
+						<div class="h-2 bg-slate-700 rounded"></div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
 </template>
 
 <style>
-@media (min-width: 1024px) {
-  .about {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
-  }
-}
+	@media (min-width: 1024px) {
+		.about {
+			min-height: 100vh;
+			display: flex;
+			align-items: center;
+		}
+	}
 </style>
