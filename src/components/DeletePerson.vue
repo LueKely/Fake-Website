3a50167c8df58eb95467696a6c12451d3c0a59a9
LueKely <luekely43110@gmail.com>
2022-11-15 11:11:44 +0800

    added delete feature

---
+++ src/components/DeletePerson.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+	import axios from 'axios';
+
+	const props = defineProps<{ id: number }>();
+	const emit = defineEmits<{ (e: 'deleteModal', status: boolean): void }>();
+
+	function hideDeleteModal() {
+		emit('deleteModal', true);
+	}
+
+	async function deleteUser(prop: number) {
+		try {
+			axios.delete(`https://reqres.in/api/users/${prop}`);
+			alert('user deleted');
+		} catch (error) {
+			alert('error occured');
+			console.log(error);
+		}
+	}
+</script>
+<template>
+	<div>
+		<h1>You are about to delete this user {{ props.id }}, are you sure?</h1>
+		<button @click="deleteUser(props.id)">Delete</button>
+		<button @click="hideDeleteModal">Cancel</button>
+	</div>
+</template>
--- src/components/PersonalDetail.vue
+++ src/components/PersonalDetail.vue
@@ -2,13 +2,21 @@
 	import { ref } from 'vue';
 	import type { PersonInfo } from '@/stores/types';
 	import UpdatePerson from './UpdatePerson.vue';
+	import DeletePerson from './DeletePerson.vue';
 	const modalStatus = ref(false);
+	const delModalStatus = ref(false);
 
 	function showModal() {
 		modalStatus.value = true;
 	}
+	function showModalDel() {
+		delModalStatus.value = true;
+	}
 	function hideModal(e: boolean) {
 		modalStatus.value = e;
+	}
+	function hideModalDel(e: boolean) {
+		delModalStatus.value = e;
 	}
 
 	const props = defineProps<{ fetchedPerson: PersonInfo }>();
@@ -24,11 +32,20 @@
 	<p>Email: {{ props.fetchedPerson.email }}</p>
 	<p>ID:{{ props.fetchedPerson.id }}</p>
 	<button @click="showModal">Update</button>
+	<button @click="showModalDel">Delete</button>
+
 	<div v-if="modalStatus == true">
 		<UpdatePerson
 			:user="props.fetchedPerson"
 			@close-update="hideModal"
 		></UpdatePerson>
+	</div>
+
+	<div v-if="delModalStatus == true">
+		<DeletePerson
+			:id="props.fetchedPerson.id"
+			@delete-modal="hideModalDel"
+		></DeletePerson>
 	</div>
 </template>
 <style></style>
