7ece2ddcea929208a39189d8e814647911b973d9
Anunciacion Lue <luekely43110@gmail.com>
2022-11-19 23:18:03 +0800

    Update HomeView.vue

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,6 +2,10 @@
 
 <template>
 	<div>
-		<div><h1>Welcome To Fake Website</h1></div>
+		<div>
+			<h1>Welcome To Fake Website</h1>
+			<p>A Website Which Is Fake</p>
+			<div><button>Get A tour</button></div>
+		</div>
 	</div>
 </template>
