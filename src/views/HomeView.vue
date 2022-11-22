083b45b4328e43b4560061c53126ebd7b77d5a5c
LueKely <luekely43110@gmail.com>
2022-11-22 09:20:14 +0800

    added gradient

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -6,19 +6,19 @@
 		<div
 			class="bg-neutral-100 w-2/5 h-9/12 py-4 px-10 flex flex-col items-center"
 		>
-			<div class="flex flex-col items-start mt-20">
-				<h1 class="text-7xl font-bold">
+			<div class="flex flex-col text-gray-900 items-start mt-20">
+				<h1 class="text-8xl font-bold">
 					Welcome To <span class="text-violet-500 font-bold">Fake Website</span>
 				</h1>
 
-				<p class="text-base w-4/5 font-light mt-5 ml-2">
+				<p class="text-xl w-4/5 font-light mt-5 ml-2">
 					A Website Which Is Fake. You can Post, Get, Patch and other stuff you
-					can do in a Rest thingy. Built with <b>Vite</b>, <b> Vue 3</b>,
-					<b> Axios</b>,<b>Tailwind</b> and <b>Pinia</b>.
+					can do in a Rest thingy. Built with <b class="">Vite</b>,
+					<b> Vue 3</b>, <b> Axios</b>,<b>Tailwind</b> and <b>Pinia</b>.
 				</p>
 				<div>
 					<button
-						class="mt-5 ml-2 font-base duration-100 bg-violet-500 font-bold text-slate-50 w-28 h-10 rounded-full transition ease-in hover:bg-neutral-100 hover:text-violet-500 hover:border-2 hover:border-violet-500 active:scale-90"
+						class="mt-6 ml-3 pb-1 font-base duration-100 bg-violet-500 font-bold text-slate-50 w-36 h-14 text-xl text-center rounded-full transition ease-in hover:bg-neutral-100 hover:text-violet-500 hover:border-2 hover:border-violet-500 active:scale-90"
 					>
 						Start a Tour
 					</button>
@@ -27,9 +27,11 @@
 		</div>
 		<!-- Picture -->
 
-		<div class="w-3/5 h-9/12 bg-WaveColor flex m-0">
+		<div
+			class="w-3/5 h-9/12 bg-WaveColor bg-violet-500 flex m-0 bg-right-bottom bg-origin-border"
+		>
 			<!-- svg -->
-			<div class="h-full w-2/5 bg-wave bg-no-repeat bg-cover"></div>
+			<div class="h-full w-5/12 bg-wave bg-no-repeat bg-cover"></div>
 		</div>
 	</div>
 </template>
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,7 @@ module.exports = {
 			backgroundImage: {
 				wave: "url('./src/assets/wave.svg')",
 				WaveColor:
-					'radial-gradient(75% 75% at 50% 50%, #8b5cf6 0%, #581c87 98%);',
+					'radial-gradient(at 18% 52%, hsla(283,81%,68%,1) 0px, transparent 50%),radial-gradient(at 79% 42%, hsla(239,81%,74%,1) 0px, transparent 50%),radial-gradient(at 90% 18%, hsla(317,72%,61%,1) 0px, transparent 50%),radial-gradient(at 32% 25%, hsla(264,86%,75%,1) 0px, transparent 50%),radial-gradient(at 2% 61%, hsla(240,95%,79%,1) 0px, transparent 50%),radial-gradient(at 92% 34%, hsla(166,88%,67%,1) 0px, transparent 50%),radial-gradient(at 87% 2%, hsla(321,74%,63%,1) 0px, transparent 50%);',
 				tite: '#f2e8e5',
 			},
 		},
