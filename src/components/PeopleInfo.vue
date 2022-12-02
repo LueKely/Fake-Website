441cdd57ba0aab3f30b6d11c1e55e8775dd27135
LueKely <luekely43110@gmail.com>
2022-12-02 11:53:04 +0800

    Bug fixed~

--- src/components/PeopleInfo.vue
+++ src/components/PeopleInfo.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-	import { onMounted, ref } from 'vue';
+	import { onMounted, computed, ref } from 'vue';
 	import type { PersonInfo } from '@/stores/types';
 	import PersonalDetail from './PersonalDetail.vue';
 	const props = defineProps<{ fetchedPerson: PersonInfo }>();
@@ -12,18 +12,35 @@
 	function hideDetails() {
 		details.value = false;
 	}
-	onMounted(async () => {});
+	onMounted(() => {});
+
+	const bgAvatar = computed(() => {
+		return { 'background-image': `url('${props.fetchedPerson.avatar}')` };
+	});
 </script>
 
 <template>
-	<div class="w-[60vw] h-[40vh] bg-slate-300 m-10">
+	<div
+		class="w-[18vw] h-[35vh] bg-slate-100 m-10 rounded-md overflow-hidden relative"
+	>
+		<div
+			class="w-[18vw] h-[40%] flex items-center justify-center bg-center bg-cover bg-no-repeat shadow-lg"
+			:style="bgAvatar"
+		>
+			<div
+				class="w-full h-full flex items-center justify-center backdrop-blur-lg"
+			></div>
+		</div>
+
 		<img
+			class="h-[100px] w-[100px] rounded-full absolute top-[23%] left-[35%] shadow-lg"
 			:src="props.fetchedPerson.avatar"
 			:alt="props.fetchedPerson.first_name"
 		/>
+
 		<!-- <h1>This is the {{ props.fetchedPerson }} info</h1> -->
 		<h1>
-			Name: {{ props.fetchedPerson.first_name }} &nbsp;{{
+			{{ props.fetchedPerson.first_name }} &nbsp;{{
 				props.fetchedPerson.last_name
 			}}
 		</h1>
