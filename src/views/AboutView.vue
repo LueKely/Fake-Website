c1c61226e7fb2bb215a374d786716dae0f1dbb61
LueKely <luekely43110@gmail.com>
2022-12-07 13:52:46 +0800

    added test notifications

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,10 +1,16 @@
 <script setup lang="ts">
 	import HttpRequestsVue from '@/components/HttpRequests.vue';
+	import notificationPopUp from '@/components/notificationPopUp.vue';
 </script>
 
 <template>
 	<h1>About</h1>
 	<HttpRequestsVue></HttpRequestsVue>
+	<div
+		class="w-screen h-screen fixed flex items-end justify-end z-50 top-0 left-0 pointer-events-none p-10"
+	>
+		<notificationPopUp></notificationPopUp>
+	</div>
 </template>
 
 <style></style>
